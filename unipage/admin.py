8011584c12e0769eb543a7cbce85cf3a2d474051
HenryHtut <thanthtutaung26@gmail.com>
2025-03-21 17:42:58 +0630

    database

--- unipage/admin.py
+++ unipage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Country)
+admin.site.register(MinimumRequirement)
+admin.site.register(TopUniversityStat)
+admin.site.register(PopularUniversity)
