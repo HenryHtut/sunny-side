54f791fb44c35ec22666fb24d75117d06879cfd7
HenryHtut <thanthtutaung26@gmail.com>
2025-04-10 22:33:14 +0630

    database

--- unipage/urls.py
+++ unipage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import country_detail_view
 
 urlpatterns = [
     path("", views.unipage, name="unipage"),
     path("landing", views.land, name="landing"),
+    path("country/<int:country_id>/", country_detail_view, name="country_detail"),
 ]
--- unipage/views.py
+++ unipage/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from .models import Country, MinimumRequirement, TopUniversityStat, PopularUniversity
 # Create your views here.
 def unipage(request):
     return render(request, "unipage/main.html")
 
 def land(request):
     return render(request, "unipage/landing.html")
+
+
+
+
+
+def country_detail_view(request):
+    countries = Country.objects.all().prefetch_related(
+        'mininum_requirements', 'top_university_stats', 'popular_universities'
+    )
+    return render(request, 'landing.html', {'countries': countries})
